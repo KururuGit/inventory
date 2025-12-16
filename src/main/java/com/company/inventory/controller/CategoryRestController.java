565240ddc4099098c2b7957d59a4e56b087a8892
KururuGit <guille.nth13@gmail.com>
2025-12-16 15:40:14 -0300

    Función delete de Categorías

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -7,6 +7,8 @@ package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
-
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1") 
 public class CategoryRestController {
@@ -46,6 +48,12 @@ public class CategoryRestController {
 	@PutMapping("/categories/{id}")
 	public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id) {
 		ResponseEntity<CategoryResponseRest> response = service.update(category, id);
+		return response;
+	}
+	
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@PathVariable Long id) {
+		ResponseEntity<CategoryResponseRest> response = service.deleteById(id);
 		return response;
 	}
 	
--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -153,4 +153,28 @@ public class CategoryServiceImpl implements ICategoryService {
 		
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> deleteById(Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		
+
+		try {
+			
+			categoryDao.deleteById(id);
+			
+			response.setMetadata("Respuesta Ok!", "200", "Categoría eliminada");
+			
+		} catch (Exception e) {
+			
+			response.setMetadata("Respuesta nok!", "-1", "Error al consultar por id");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
 }
